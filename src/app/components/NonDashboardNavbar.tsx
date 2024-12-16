976753a9dc2d1c3d0cfc9291ce6efe7ae8127536
hamzatalha77 <hamza_ta7@hotmail.com>
2024-12-16 12:18:27 +0100

    commit

--- src/app/components/NonDashboardNavbar.tsx
+++ src/app/components/NonDashboardNavbar.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const NonDashboardNavbar = () => {
-  return <nav className="nondashboard-navbar">hello</nav>
+  return (
+    <nav className="nondashboard-navbar">
+      <div className="nondashbaord-navbar__container"></div>
+    </nav>
+  )
 }
 
 export default NonDashboardNavbar
