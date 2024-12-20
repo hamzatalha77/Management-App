1d347f5dcac29ea30f62775eeda913e1e5f9c90c
hamzatalha77 <hamza_ta7@hotmail.com>
2024-12-20 13:08:11 +0100

    commit

--- src/app/components/NonDashboardNavbar.tsx
+++ src/app/components/NonDashboardNavbar.tsx
@@ -6,24 +6,31 @@ const NonDashboardNavbar = () => {
   return (
     <nav className="nondashboard-navbar">
       <div className="nondashboard-navbar__container">
-        <Link href="/" className="nondashboard-navbar__brand">
-          GOTEI
-        </Link>
-        <div className="flex items-center gap-4">
-          <div className="relative group">
-            <Link href="/search" className="nondashboard-navbar__search-input">
-              <span className="hidden sm:inline">Search Courses</span>
-              <span className="sm:hidden">Search</span>
-            </Link>
-            <BookOpen className="nondashboard-navbar__search-icon" size={18} />
+        <div className="nondashboard-navbar__search">
+          <Link href="/" className="nondashboard-navbar__brand">
+            GOTEI
+          </Link>
+          <div className="flex items-center gap-4">
+            <div className="relative group">
+              <Link
+                href="/search"
+                className="nondashboard-navbar__search-input"
+              >
+                <span className="hidden sm:inline">Search Courses</span>
+                <span className="sm:hidden">Search</span>
+              </Link>
+              <BookOpen
+                className="nondashboard-navbar__search-icon"
+                size={18}
+              />
+            </div>
           </div>
         </div>
       </div>
       <div className="nondashboard-navbar__actions">
-        <button className="nondashboard-navbar__notifications-button">
-          <span className="nondashboard-navbar__notifications-indicator">
-            <Bell className="nondashboard-navbar__notifications-icon" />
-          </span>
+        <button className="nondashboard-navbar__notification-button">
+          <span className="nondashboard-navbar__notification-indicator"> </span>
+          <Bell className="nondashboard-navbar__notification-icon" />
         </button>
       </div>
     </nav>
