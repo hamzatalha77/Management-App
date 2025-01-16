f27af4c93513e74a380918c0b7eed1c78357182d
hamzatalha77 <hamza_ta7@hotmail.com>
2025-01-16 11:12:24 +0100

    commit

--- src/app/state/api.ts
+++ src/app/state/api.ts
@@ -3,8 +3,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: 'api',
-  tagTypes: [],
-  endpoints: (build) => ({})
+  tagTypes: ['Courses'],
+  endpoints: (build) => ({
+    getCourses: build.query<Course[], { category?: string }>({
+      query: ({ category }) => ({
+        url: 'courses',
+        params: { category }
+      }),
+      providesTags: ['Courses']
+    })
+  })
 })
 
 export const {} = api
