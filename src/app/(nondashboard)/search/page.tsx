c52cc94a7a6b0a86ba98c438eb3681b47dd400f0
hamzatalha77 <hamza_ta7@hotmail.com>
2025-01-20 13:50:00 +0100

    commit

--- src/app/(nondashboard)/search/page.tsx
+++ src/app/(nondashboard)/search/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useSearchParams } from 'next/navigation'
+import { useGetCoursesQuery } from '@/app/state/api'
 
 const Search = () => {
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id')
+  const { data: courses, isLoading, isError } = useGetCoursesQuery()
   return <div></div>
 }
 
