d339ab412dab7613f7961ac4ef0690c027532277
hamzatalha77 <hamza_ta7@hotmail.com>
2025-01-21 12:18:08 +0100

    commit

--- src/app/(nondashboard)/search/page.tsx
+++ src/app/(nondashboard)/search/page.tsx
@@ -58,6 +58,14 @@ const Search = () => {
             />
           ))}
         </motion.div>
+        {selectedCourse && (
+          <motion.div
+            initial={{ y: 40, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className="search__selected-course"
+          ></motion.div>
+        )}
       </div>
     </motion.div>
   )
