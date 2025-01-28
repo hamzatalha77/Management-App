6a88d96b96c0fd2397c8b77e06d1e5a010ca9445
hamzatalha77 <hamza_ta7@hotmail.com>
2025-01-28 11:44:14 +0100

    commit

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,30 @@
+'use client'
+import Navbar from '@/components/Navbar'
+import { useUser } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
+import { useState } from 'react'
+import Loading from '../components/Loading'
+import { cn } from '../lib/utils'
+import { SidebarProvider } from '../components/ui/sidebar'
+import AppSidebar from '../components/AppSidebar'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  const [courseId, setCourseId] = useState<string | null>(null)
+  const [user, isLoaded] = useUser()
+
+  if (!isLoaded) return <Loading />
+  if (!user) return <div>Please sign in to access this page.</div>
+  return (
+    <SidebarProvider>
+      <div className="dashboard">
+        <AppSidebar />
+        <div className="dashboard__content">
+          <div className={cn('dashboard__main')} style={{ height: '100vh' }}>
+            <main className="nondashboard-layout__main">{children}</main>
+          </div>
+        </div>
+      </div>
+    </SidebarProvider>
+  )
+}
