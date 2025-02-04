a5dfbfc3a8428531aa49f7503d422123333a7838
hamzatalha77 <hamza_ta7@hotmail.com>
2025-02-04 12:02:39 +0100

    commit

--- src/app/(dashboard)/user/settings/page.tsx
+++ src/app/(dashboard)/user/settings/page.tsx
@@ -1,7 +1,12 @@
+import SharedNotificationSettings from '@/app/components/SharedNotificationSettings'
 import React from 'react'
 
 const UserSettings = () => {
-  return <div></div>
+  return (
+    <div className="w-3/5">
+      <SharedNotificationSettings />
+    </div>
+  )
 }
 
 export default UserSettings
