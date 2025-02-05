47384e0451b287749385a3bfa6f561cb343c4c0b
hamzatalha77 <hamza_ta7@hotmail.com>
2025-02-05 12:40:50 +0100

    commit

--- src/app/components/SharedNotificationSettings.tsx
+++ src/app/components/SharedNotificationSettings.tsx
@@ -1,6 +1,27 @@
+import { useUser } from '@clerk/nextjs'
 import React from 'react'
+import { useUpdateUserMutation } from '../state/api'
+import {
+  NotificationSettingsFormData,
+  notificationSettingsSchema
+} from '../lib/schemas'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-const SharedNotificationSettings = () => {
+const SharedNotificationSettings = ({
+  title = 'Notification Setting',
+  subtitle = 'Manage your notification settings'
+}: SharedNotificationSettingsProps) => {
+  const { user } = useUser()
+  const { updateUser } = useUpdateUserMutation()
+  const currentSettings =
+    (user?.publicMetadata as { settings?: UserSettings })?.settings || {}
+  const methods = useForm<NotificationSettingsFormData>({
+    resolver: zodResolver(notificationSettingsSchema),
+    defaultValues: {
+      courseNotifications
+    }
+  })
   return <div></div>
 }
 
